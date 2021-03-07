fb345e7d003c4f227a769cde68ddb9c456b9c839
Holden Greene <holdengreene26@gmail.com>
2021-03-07 15:17:53 -0700

    Ignore postcss dependbot for now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  purge: [
-    './components/**/*.{vue,js}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
-  ],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
